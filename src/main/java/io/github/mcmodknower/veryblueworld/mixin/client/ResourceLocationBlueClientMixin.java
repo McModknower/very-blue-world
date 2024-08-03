714af78b20586676b595fa6d09442a7802ac2860
McModknower <teetoll@t-online.de>
2024-08-03 14:29:26 +0200

    Use minecraft NativeImage instead of java awt to be mac-compatible.

--- src/main/java/io/github/mcmodknower/veryblueworld/mixin/client/ResourceLocationBlueClientMixin.java
+++ src/main/java/io/github/mcmodknower/veryblueworld/mixin/client/ResourceLocationBlueClientMixin.java
@@ -1,22 +1,24 @@
 package io.github.mcmodknower.veryblueworld.mixin.client;
 
+import net.minecraft.client.texture.NativeImage;
 import net.minecraft.resource.Resource;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.geom.AffineTransform;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.Objects;
 
 @Mixin(Resource.class)
 public abstract class ResourceLocationBlueClientMixin {
+
+	@Unique
+	private static final int BLUE = NativeImage.packColor(0,0xFF, 0,0);
+	@Unique
+	private static final int ALPHA_MASK = NativeImage.packColor(0xFF,0, 0,0);
+
 	@Inject(at = @At("RETURN"), method = "getInputStream()Ljava/io/InputStream;", cancellable = true)
 	private void getInputStream(CallbackInfoReturnable<InputStream> cir) throws IOException {
 
@@ -58,5 +60,20 @@ public abstract class ResourceLocationBlueClientMixin {
 		byte[] data = byteOut.toByteArray();
 		ByteArrayInputStream inputStream = new ByteArrayInputStream(data);
 		cir.setReturnValue(inputStream);
+		try(NativeImage image = NativeImage.read(new ByteArrayInputStream(original));) {
+			int width = image.getWidth();
+			int height = image.getHeight();
+			for (int x = 0; x < width; x++) {
+				for (int y = 0; y < height; y++) {
+					int color = image.getColor(x,y);
+					image.setColor(x,y,(color & ALPHA_MASK) | BLUE);
+				}
+			}
+			byte[] newImage = image.getBytes();
+			ByteArrayInputStream inputStream = new ByteArrayInputStream(newImage);
+			cir.setReturnValue(inputStream);
+		} catch (IOException e) {
+			cir.setReturnValue(new ByteArrayInputStream(original));
+		}
 	}
 }
